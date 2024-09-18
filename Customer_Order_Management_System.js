cb598fb8d3318e9d845dd83d4f18ada32b099e63
Alexander Curtis <ajcurtis@usf.edu>
2024-09-18 17:59:50 -0400

    Initialize orders array

--- Customer_Order_Management_System.js
+++ Customer_Order_Management_System.js
@@ -1,6 +1,22 @@
+//Create an Inventory Array of Product Objects
+
 let inventory = [ 
     { name: 'Coffee', price: 5, quantity: 20 },
     { name: 'Tea', price: 3, quantity: 15},
     { name: 'Muffin', price: 2, quantity: 50},
     { name: 'Donut', price: 3, quantity: 25}
 ];
+
+//Create an Orders Array of Order Objects
+
+let orders = [
+    {
+        customerName: "",
+        items: [
+            { name: "", quantity: 0}
+        ],
+        status: ""
+    }
+];
+
+
