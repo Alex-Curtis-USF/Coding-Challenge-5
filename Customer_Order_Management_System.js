b22e7c90a2b814d292e60799377e5b3a3e36ed83
Alexander Curtis <ajcurtis@usf.edu>
2024-09-18 15:43:50 -0400

    Initialize inventory with product objects

---
+++ Customer_Order_Management_System.js
@@ -0,0 +1,6 @@
+let inventory = [ 
+    { name: 'Coffee', price: 5, quantity: 20 },
+    { name: 'Tea', price: 3, quantity: 15},
+    { name: 'Muffin', price: 2, quantity: 50},
+    { name: 'Donut', price: 3, quantity: 25}
+];
