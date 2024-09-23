60ebf91ec34143bc942f36adbeb2045a851954c8
Alexander Curtis <ajcurtis@usf.edu>
2024-09-22 20:58:07 -0400

    Create calculateOrderTotal function

--- Customer_Order_Management_System.js
+++ Customer_Order_Management_System.js
@@ -1,5 +1,5 @@
 //Create an Inventory Array of Product Objects
-
+// Already ordered
 let inventory = [ 
     { name: 'Coffee', price: 5, quantity: 20 },
     { name: 'Tea', price: 3, quantity: 15},
@@ -9,14 +9,58 @@ let inventory = [
 
 //Create an Orders Array of Order Objects
 
-let orders = [
-    {
-        customerName: "",
-        items: [
-            { name: "", quantity: 0}
-        ],
-        status: ""
+let orders = [];
+
+// Task 3
+
+itemsOrdered = [
+    { name: 'Coffee', quantity: 1},
+    { name: 'Tea', quantity: 1}
+]
+
+//Create a Function to Place an Order
+
+function placeOrder(customerName, itemsOrdered) {
+    let orderError = false;
+
+    // element is every element of items ordered
+    itemsOrdered.forEach(itemToOrder => {
+        // inv is every element of inventory
+        let invResult = inventory.find((itemInInv)=>
+            itemInInv.name === itemToOrder.name
+        )
+
+        if (!invResult || 0 > (invResult.quantity - itemToOrder.quantity)) {
+            orderError = true;
+            return;
+        }
+        invResult.quantity -= itemToOrder.quantity;
+    });
+
+    if (orderError) {
+        console.log("Order can not be processed.")
+        return;
     }
-];
+    orders.push({
+        costumerName: customerName,
+        items: itemsOrdered,
+        status: "Pending"
+    })
+    console.log(orders);
 
+}
 
+placeOrder("James", itemsOrdered);
+
+// Create a Function to Calculate Total for an Order
+
+function calculateOrderTotal(itemsOrdered) {
+    return itemsOrdered.reduce((total, item) => {
+        let inventoryItem = inventory.find(invItem => invItem.name === item.name);
+        return total + (inventoryItem ? inventoryItem.price * item.quantity : 0);
+    }, 0);
+}
+
+console.log("Total price of the order: $" + calculateOrderTotal(itemsOrdered));
+
+// 
